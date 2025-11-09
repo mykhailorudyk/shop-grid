a4b8a206bc1a1422c5f9008a4da9bb88a3daef8f
Mykhailo Rudyk <m.rudyk@knubisoft.com>
2025-11-09 02:51:35 +0200

    implemented custom login form for frontend

--- users-service/src/main/java/com/rudyk/shopgrid/usersservice/security/SecurityConfig.java
+++ users-service/src/main/java/com/rudyk/shopgrid/usersservice/security/SecurityConfig.java
@@ -21,6 +21,7 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(HttpMethod.POST, "/api/v1/users/register").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/v1/users").hasRole("ADMIN")
                 .requestMatchers(HttpMethod.DELETE, "/api/v1/users/**").hasRole("ADMIN")
                 .requestMatchers(HttpMethod.GET, "/api/v1/users/**").hasAnyRole("USER", "ADMIN")
---
+++ users-service/src/main/java/com/rudyk/shopgrid/usersservice/service/LoginService.java
@@ -0,0 +1,9 @@
+package com.rudyk.shopgrid.usersservice.service;
+
+import com.rudyk.shopgrid.usersservice.dto.LoginRequestDto;
+
+public interface LoginService {
+
+    Object login(LoginRequestDto loginRequestDto);
+
+}
